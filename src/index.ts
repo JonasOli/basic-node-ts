9df37b73ff970410f780e48e8eb91ebb5e87d83f
Jonas Oliveira <jonasfissicaro13@gmail.com>
2024-05-12 12:13:12 -0300

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express, { Express, Request, Response } from "express";
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
 
+dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
